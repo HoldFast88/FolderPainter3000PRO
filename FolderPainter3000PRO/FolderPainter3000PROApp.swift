e020e28b92bfff82b81dc830e8590978b49033fc
Oleksii Voitenko <oleksii.voitenko@jmind.systems>
2023-05-05 17:10:10 +0300

    Initial Commit

---
+++ FolderPainter3000PRO/FolderPainter3000PROApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderPainter3000PROApp.swift
+//  FolderPainter3000PRO
+//
+//  Created by Oleksii Voitenko on 05.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FolderPainter3000PROApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
